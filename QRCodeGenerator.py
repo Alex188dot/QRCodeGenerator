6a3c94d6243cc6f81c8197d5a5b8492a692d6a62
alessio.leodori <alessio.leodori@gmail.com>
2023-07-08 14:39:03 +0200

    Added dark (actual qr code) and light (background) option for colors

--- QRCodeGenerator.py
+++ QRCodeGenerator.py
@@ -1,5 +1,5 @@
 import segno
 
-price_tag = segno.make("https://github.com/Alex188dot")
-price_tag.save("Alessio's_Github.png", scale=10)
+qr_code = segno.make("https://github.com/Alex188dot")
+qr_code.save("Alessio's_Github.png", dark="#ffb10b", light="#f7ff0b", scale=10)
 print('QR created successfully!')
